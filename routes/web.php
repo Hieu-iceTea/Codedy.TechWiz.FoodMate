9cef4326a122d0a0ea6126b577163bb709eee346
Nguyễn Đình Hiếu <Hieu.iceTea@gmail.com>
2021-04-23 19:04:14 +0700

    NEW: ./index: Hiển thị trang chủ

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+//Route::get('/', function () {
+//    return view('welcome');
+//});
+
+Route::prefix('')->group(function () {
+    Route::get('', [App\Http\Controllers\Front\HomeController::class, 'index']);
 });
