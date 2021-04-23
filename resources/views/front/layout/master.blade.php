eb8e7ccf84f4e35f13e7975ce2c5451e8ce053b6
Nguyễn Đình Hiếu <Hieu.iceTea@gmail.com>
2021-04-23 20:10:27 +0700

    UPDATE: Menu_bar

--- resources/views/front/layout/master.blade.php
+++ resources/views/front/layout/master.blade.php
@@ -54,72 +54,30 @@
                     <!-- Navigation -->
                     <nav class="module module-navigation left mr-4">
                         <ul id="nav-main" class="nav nav-main">
-                            <li class="has-dropdown">
-                                <a href="#">Home</a>
-                                <div class="dropdown-container">
-                                    <ul>
-                                        <li><a href="index.html">Home Basic</a></li>
-                                        <li><a href="index-burgers.html">Home Burgers</a></li>
-                                        <li><a href="index-slider.html">Home Fullwidth Slider</a></li>
-                                        <li><a href="index-video.html">Home Video</a></li>
-                                        <li><a href="index-fresh.html">Home Fresh <span
-                                                    class="badge badge-success">New</span></a></li>
-                                        <li><a href="index-dark.html">Home Dark <span
-                                                    class="badge badge-success">New</span></a></li>
-                                    </ul>
-                                </div>
-                            </li>
+                            <li><a href="../">Home</a></li>
                             <li class="has-dropdown">
                                 <a href="#">About</a>
                                 <div class="dropdown-container">
                                     <ul class="dropdown-mega">
-                                        <li><a href="page-about.html">About Us</a></li>
-                                        <li><a href="page-services.html">Services</a></li>
-                                        <li><a href="page-gallery.html">Gallery</a></li>
-                                        <li><a href="page-reviews.html">Reviews</a></li>
-                                        <li><a href="page-faq.html">FAQ</a></li>
+                                        <li><a href="../about">About Us</a></li>
+                                        <li><a href="../services">Services</a></li>
+                                        <li><a href="../faq">FAQ</a></li>
                                     </ul>
                                     <div class="dropdown-image">
                                         <img src="data-images/photos/dropdown-about.jpg" alt="">
                                     </div>
                                 </div>
                             </li>
-                            <li class="has-dropdown">
-                                <a href="#">Menu</a>
-                                <div class="dropdown-container">
-                                    <ul>
-                                        <li class="has-dropdown">
-                                            <a href="#">List</a>
-                                            <ul>
-                                                <li><a href="menu-list-navigation.html">Navigation</a></li>
-                                                <li><a href="menu-list-collapse.html">Collapse</a></li>
-                                            </ul>
-                                        </li>
-                                        <li class="has-dropdown">
-                                            <a href="#">Grid</a>
-                                            <ul>
-                                                <li><a href="menu-grid-navigation.html">Navigation</a></li>
-                                                <li><a href="menu-grid-collapse.html">Collapse</a></li>
-                                            </ul>
-                                        </li>
-                                    </ul>
-                                </div>
-                            </li>
-                            <li><a href="page-offers.html">Offers</a></li>
-                            <li><a href="page-contact.html">Contact</a></li>
+                            <li><a href="../menu">Menu</a></li>
+                            <li><a href="../restaurant">Restaurant</a></li>
+                            <li><a href="../contact">Contact</a></li>
                             <li class="has-dropdown">
                                 <a href="#">More</a>
                                 <div class="dropdown-container">
                                     <ul class="dropdown-mega">
-                                        <li><a href="page-offer-single.html">Offer single</a></li>
-                                        <li><a href="page-product.html">Product</a></li>
-                                        <li><a href="book-a-table.html">Book a table</a></li>
-                                        <li><a href="checkout.html">Checkout</a></li>
-                                        <li><a href="confirmation.html">Confirmation</a></li>
-                                        <li><a href="blog.html">Blog</a></li>
-                                        <li><a href="blog-sidebar.html">Blog + Sidebar</a></li>
-                                        <li><a href="blog-post.html">Blog Post</a></li>
-                                        <li><a href="documentation/index.html" target="_blank">Documentation</a></li>
+                                        <li><a href="../book-a-table">Book a table</a></li>
+                                        <li><a href="../checkout">Checkout</a></li>
+                                        <li><a href="../offer">Offers</a></li>
                                     </ul>
                                     <div class="dropdown-image">
                                         <img src="data-images/photos/dropdown-more.jpg" alt="">
