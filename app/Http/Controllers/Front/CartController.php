0053ff8ff870ef8162b7d538037d52f0e9cce8bd
Nguyễn Đình Hiếu <Hieu.iceTea@gmail.com>
2021-04-24 15:16:09 +0700

    NEW: ./Cart/add

---
+++ app/Http/Controllers/Front/CartController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Controllers\Front;
+
+use App\Http\Controllers\Controller;
+use App\Models\Product;
+use Illuminate\Http\Request;
+use Gloudemans\Shoppingcart\Facades\Cart;
+
+class CartController extends Controller
+{
+    public function add ($id) {
+        $product = Product::findOrFail($id);
+
+        Cart::add([
+            'id' => $id,
+            'name' => $product->name,
+            'qty' => 1,
+            'price' => $product->discount ?? $product->price,
+            'weight' => $product->weight ?? 0,
+            'options' => [
+                'image' => $product->image,
+            ],
+        ]);
+
+        dd(Cart::content());
+
+        return back();
+    }
+
+    public function index() {
+        $carts = Cart::content();
+        $total = Cart::total();
+        $subtotal = Cart::subtotal();
+
+        return view('front.cart', compact('carts', 'total', 'subtotal'));
+    }
+
+    public function delete($rowId) {
+        Cart::remove($rowId);
+
+        return back();
+    }
+
+    public function destroy() {
+        Cart::destroy();
+
+        return back();
+    }
+
+    public function update(Request $request) {
+        if ($request->ajax()) {
+            Cart::update($request->rowId, $request->qty);
+        }
+    }
+}
