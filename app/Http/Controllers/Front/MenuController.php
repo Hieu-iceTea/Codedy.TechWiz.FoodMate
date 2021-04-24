d3d980fdd88a31c1b16322aa994a6a4d12ede96c
Nguyễn Đình Hiếu <Hieu.iceTea@gmail.com>
2021-04-24 09:27:53 +0700

    NEW: ./menu

--- app/Http/Controllers/Front/MenuController.php
+++ app/Http/Controllers/Front/MenuController.php
@@ -4,13 +4,17 @@ namespace App\Http\Controllers\Front;
 
 use App\Http\Controllers\Controller;
 use App\Models\Product;
+use App\Models\ProductCategory;
 use Illuminate\Http\Request;
 
 class MenuController extends Controller
 {
     public function index()
     {
-        return view('front.menu.index');
+        $categories = ProductCategory::all();
+        $products = Product::all();
+
+        return view('front.menu.index', compact('categories', 'products'));
     }
 
     public function show($id)
