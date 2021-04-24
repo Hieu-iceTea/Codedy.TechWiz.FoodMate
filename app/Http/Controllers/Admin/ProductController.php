1b5aea475c0ceafab28a831eef3d889683c646d6
hungdaycb00 <60802128+hungdaycb00@users.noreply.github.com>
2021-04-24 18:35:59 +0700

    Update ProductController.php

--- app/Http/Controllers/Admin/ProductController.php
+++ app/Http/Controllers/Admin/ProductController.php
@@ -97,7 +97,7 @@ class ProductController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(ProductRequest $request, $id)
     {
         $data = $request->all();
 
