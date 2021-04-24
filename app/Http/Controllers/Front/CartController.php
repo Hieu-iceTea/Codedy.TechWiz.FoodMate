9b4bb24b59c30d8c88bff8c0534b5d2c587f7dec
Nguyễn Đình Hiếu <Hieu.iceTea@gmail.com>
2021-04-24 20:31:38 +0700

    NEW: ./Cart/index

--- app/Http/Controllers/Front/CartController.php
+++ app/Http/Controllers/Front/CartController.php
@@ -23,9 +23,7 @@ class CartController extends Controller
             ],
         ]);
 
-        dd(Cart::content());
-
-        return back();
+        return redirect('cart');
     }
 
     public function index() {
