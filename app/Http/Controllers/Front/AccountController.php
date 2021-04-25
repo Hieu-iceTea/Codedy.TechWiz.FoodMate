7ed3a9fc711d6600e239522ed8e10a268d42ec85
Nguyễn Đình Hiếu <Hieu.iceTea@gmail.com>
2021-04-25 14:30:21 +0700

    NEW: giao diện tĩnh trang login, registration

--- app/Http/Controllers/Front/AccountController.php
+++ app/Http/Controllers/Front/AccountController.php
@@ -3,17 +3,65 @@
 namespace App\Http\Controllers\Front;
 
 use App\Http\Controllers\Controller;
+use App\Utilities\Constant;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AccountController extends Controller
 {
-    public function index()
+    public function login()
     {
-        return view('front.account.my-order.index');
+        return view('front.account.login');
     }
 
-    public function show($id)
+    public function checkLogin(Request $request)
     {
-        return view('front.account.my-order.show');
+        $credentials = [
+            'email' => $request->email,
+            'password' => $request->password,
+            'level' => Constant::user_level_client, //Tài khoản cấp độ khách hàng bình thường.
+        ];
+
+        $remember = $request->remember;
+
+        if (Auth::attempt($credentials, $remember)) {
+            //return redirect(''); //trang chủ
+            return redirect()->intended(''); //Mặc định là: trang chủ
+        } else {
+            return back()
+                ->with('notification', 'ERROR: Email or password is wrong');
+        }
+    }
+
+    public function logout()
+    {
+        Auth::logout();
+
+        return back();
+    }
+
+    public function register()
+    {
+        return view('front.account.register');
+    }
+
+    public function postRegister(Request $request)
+    {
+        if ($request->password != $request->password_confirmation) {
+            return back()
+                ->with('notification', 'ERROR: Confirm password does not match');
+        }
+
+        $data = [
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password),
+            'level' => Constant::user_level_client, //đăng ký tài khoản cấp: khách hàng bình thường.
+        ];
+
+        $this->userService->create($data);
+
+        return redirect('account/login')
+            ->with('notification', 'Register Success! Please login.');
     }
 }
---
+++ resources/views/front/account/login.blade.php
@@ -0,0 +1,128 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <base href="/dashboard/">
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta http-equiv="Content-Language" content="en">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title>Login - ArchitectUI HTML Bootstrap 4 Dashboard Template</title>
+    <meta name="viewport"
+          content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no, shrink-to-fit=no"/>
+    <meta name="description" content="ArchitectUI HTML Bootstrap 4 Dashboard Template">
+
+    <!-- Disable tap highlight on IE -->
+    <meta name="msapplication-tap-highlight" content="no">
+
+    <link href="./main.css" rel="stylesheet">
+</head>
+
+<body>
+<div class="app-container app-theme-white body-tabs-shadow">
+    <div class="app-container">
+        <div class="h-100">
+            <div class="h-100 no-gutters row">
+                <div class="d-none d-lg-block col-lg-4">
+                    <div class="slider-light">
+                        <div class="slick-slider">
+                            <div>
+                                <div
+                                    class="position-relative h-100 d-flex justify-content-center align-items-center bg-plum-plate"
+                                    tabindex="-1">
+                                    <div class="slide-img-bg"
+                                         style="background-image: url('assets/images/originals/food-1.jpg');"></div>
+                                    <div class="slider-content">
+                                        <h3>Crispy Baked Falafel</h3>
+                                        <p>
+                                            We had them with whole wheat pita, hummus, vegan pesto mayo, tomatoes,
+                                            kalamata olives and spring greens.
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div>
+                                <div
+                                    class="position-relative h-100 d-flex justify-content-center align-items-center bg-premium-dark"
+                                    tabindex="-1">
+                                    <div class="slide-img-bg"
+                                         style="background-image: url('assets/images/originals/food-2.jpg');"></div>
+                                    <div class="slider-content">
+                                        <h3>Black Bean Sweet Potato Enchiladas</h3>
+                                        <p>
+                                            I paired it with Cilantro Lime Rice – perfection. I’m going to be whipping
+                                            up more of your recipes this coming week. Thank you, Kate!
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div>
+                                <div
+                                    class="position-relative h-100 d-flex justify-content-center align-items-center bg-sunny-morning"
+                                    tabindex="-1">
+                                    <div class="slide-img-bg"
+                                         style="background-image: url('assets/images/originals/food-3.jpg');"></div>
+                                    <div class="slider-content">
+                                        <h3>Best Lentil Soup</h3>
+                                        <p>
+                                            This is one of my favorite recipes ever. I think I could eat it every day.
+                                            After every single bite. The curry powder and lemon
+                                            juice MAKE IT.
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="h-100 d-flex bg-white justify-content-center align-items-center col-md-12 col-lg-8">
+                    <div class="mx-auto app-login-box col-sm-12 col-md-10 col-lg-9">
+                        <div class="app-logo"></div>
+                        <h4 class="mb-0">
+                            <span class="d-block">Welcome back,</span>
+                            <span>Please sign in to your account.</span>
+                        </h4>
+                        <h6 class="mt-3">No account? <a href="../account/register" class="text-primary">Sign up now</a>
+                        </h6>
+                        <div class="divider row"></div>
+                        <div>
+                            <form class="">
+                                <div class="form-row">
+                                    <div class="col-md-6">
+                                        <div class="position-relative form-group">
+                                            <label for="exampleEmail" class="">Email</label>
+                                            <input name="email" id="exampleEmail" placeholder="Email here..."
+                                                   type="email" class="form-control">
+                                        </div>
+                                    </div>
+                                    <div class="col-md-6">
+                                        <div class="position-relative form-group">
+                                            <label for="examplePassword" class="">Password</label>
+                                            <input name="password" id="examplePassword" placeholder="Password here..."
+                                                   type="password" class="form-control">
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="position-relative form-check">
+                                    <input name="check" id="exampleCheck" type="checkbox" class="form-check-input">
+                                    <label for="exampleCheck" class="form-check-label">Keep me logged in</label>
+                                </div>
+                                <div class="divider row"></div>
+                                <div class="d-flex align-items-center">
+                                    <div class="ml-auto">
+                                        <a href="javascript:void(0);" class="btn-lg btn btn-link">Recover Password</a>
+                                        <button class="btn btn-primary btn-lg">Login to Dashboard</button>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<script type="text/javascript" src="./assets/scripts/main.js"></script>
+</body>
+
+</html>
