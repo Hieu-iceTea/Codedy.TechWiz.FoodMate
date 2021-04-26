93c638b86c7972779725d59c38a5d1577bcf53c6
vuquanghuy2001 <59812614+vuquanghuy2001@users.noreply.github.com>
2021-04-26 14:59:01 +0700

    NEW: trang report trong admin

---
+++ resources/views/admin/report/index.blade.php
@@ -0,0 +1,137 @@
+@extends('admin.layout.master')
+
+@section('title', 'Report')
+
+@section('body')
+
+    <div class="app-main__inner">
+        <div class="app-page-title">
+            <div class="page-title-wrapper">
+                <div class="page-title-heading">
+                    <div class="page-title-icon">
+                        <i class="pe-7s-medal icon-gradient bg-tempting-azure"></i>
+                    </div>
+                    <div>Report table
+                        <div class="page-title-subheading">restaurant income statement
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="btn-actions-pane-right mb-3">
+            <div role="group" class="btn-group-sm btn-group">
+                <button class="btn btn-primary">This month</button>
+                <button class="active btn btn-success">Last Month</button>
+                <button class="active btn btn-warning">This Year</button>
+                <button class="active btn btn-info">Anytime</button>
+            </div>
+        </div>
+
+        <div class="main-card mb-3 card">
+            <div class="card-body">
+                <table style="width: 100%;" id="" class="table table-hover table-striped table-bordered">
+                    <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Position</th>
+                        <th>Office</th>
+                        <th>Age</th>
+                        <th>Start date</th>
+                        <th>Salary</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr>
+                        <td>Tiger Nixon</td>
+                        <td>System Architect</td>
+                        <td>Edinburgh</td>
+                        <td>61</td>
+                        <td>2011/04/25</td>
+                        <td>$320,800</td>
+                    </tr>
+                    <tr>
+                        <td>Garrett Winters</td>
+                        <td>Accountant</td>
+                        <td>Tokyo</td>
+                        <td>63</td>
+                        <td>2011/07/25</td>
+                        <td>$170,750</td>
+                    </tr>
+                    <tr>
+                        <td>Ashton Cox</td>
+                        <td>Junior Technical Author</td>
+                        <td>San Francisco</td>
+                        <td>66</td>
+                        <td>2009/01/12</td>
+                        <td>$86,000</td>
+                    </tr>
+                    </tbody>
+                    <tfoot>
+                    <tr>
+                        <th>Name</th>
+                        <th>Position</th>
+                        <th>Office</th>
+                        <th>Age</th>
+                        <th>Start date</th>
+                        <th>Salary</th>
+                    </tr>
+                    </tfoot>
+                </table>
+            </div>
+
+
+
+            <div class="card-body">
+                <table style="width: 100%;" id="" class="table table-hover table-striped table-bordered">
+                    <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Position</th>
+                        <th>Office</th>
+                        <th>Age</th>
+                        <th>Start date</th>
+                        <th>Salary</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr>
+                        <td>Tiger Nixon</td>
+                        <td>System Architect</td>
+                        <td>Edinburgh</td>
+                        <td>61</td>
+                        <td>2011/04/25</td>
+                        <td>$320,800</td>
+                    </tr>
+                    <tr>
+                        <td>Garrett Winters</td>
+                        <td>Accountant</td>
+                        <td>Tokyo</td>
+                        <td>63</td>
+                        <td>2011/07/25</td>
+                        <td>$170,750</td>
+                    </tr>
+                    <tr>
+                        <td>Ashton Cox</td>
+                        <td>Junior Technical Author</td>
+                        <td>San Francisco</td>
+                        <td>66</td>
+                        <td>2009/01/12</td>
+                        <td>$86,000</td>
+                    </tr>
+                    </tbody>
+                    <tfoot>
+                    <tr>
+                        <th>Name</th>
+                        <th>Position</th>
+                        <th>Office</th>
+                        <th>Age</th>
+                        <th>Start date</th>
+                        <th>Salary</th>
+                    </tr>
+                    </tfoot>
+                </table>
+            </div>
+        </div>
+    </div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -87,16 +87,17 @@ Route::prefix('')->middleware('CheckMemberLogin')->group(function () {
 |
 */
 
-    
+
 Route::prefix('admin')->middleware('CheckAdminLogin')->group(function () {
     Route::get('/', [App\Http\Controllers\admin\HomeController::class, 'index']);
-  
+
     Route::resource('category', App\Http\Controllers\Admin\CategoryController::class);
     Route::resource('order', App\Http\Controllers\Admin\OrderController::class);
     Route::resource('product', App\Http\Controllers\Admin\ProductController::class);
     Route::resource('restaurant', App\Http\Controllers\Admin\RestaurantController::class);
     Route::resource('user', App\Http\Controllers\Admin\UserController::class);
     Route::resource('contact', App\Http\Controllers\Admin\ContactController::class);
+    Route::resource('report', App\Http\Controllers\Admin\ReportController::class);
 
     Route::prefix('login')->group(function () {
         Route::get('', [\App\Http\Controllers\Admin\HomeController::class, 'getLogin'])->withoutMiddleware('CheckAdminLogin');
