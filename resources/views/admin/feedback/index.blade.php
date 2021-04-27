f9d3a3547575881b6f3ee5dbbddb0577abb9896e
trunganh98 <60956094+trunganh98@users.noreply.github.com>
2021-04-27 11:56:20 +0700

    UPDATE: xoá nút delete và edit

--- resources/views/admin/feedback/index.blade.php
+++ resources/views/admin/feedback/index.blade.php
@@ -19,16 +19,6 @@
                         </div>
                     </div>
                 </div>
-
-                <div class="page-title-actions">
-                    <a href="{{ url()->current() . '/create' }}"
-                       class="btn-shadow btn-hover-shine mr-3 btn btn-primary">
-                                    <span class="btn-icon-wrapper pr-2 opacity-7">
-                                        <i class="fa fa-plus fa-w-20"></i>
-                                    </span>
-                        Create
-                    </a>
-                </div>
             </div>
         </div>
 
@@ -84,26 +74,6 @@
                                            class="btn btn-hover-shine btn-outline-primary border-0 btn-sm">
                                             Details
                                         </a>
-                                        <a href="{{ url()->current() . '/' . $feedback->id . '/edit'}}"
-                                           data-toggle="tooltip" title="Edit"
-                                           data-placement="bottom" class="btn btn-outline-warning border-0 btn-sm">
-                                                        <span class="btn-icon-wrapper opacity-8">
-                                                            <i class="fa fa-edit fa-w-20"></i>
-                                                        </span>
-                                        </a>
-                                        <form class="d-inline" action="{{ url()->current() . '/' . $feedback->id }}"
-                                              method="post">
-                                            @csrf
-                                            @method('DELETE')
-                                            <button class="btn btn-hover-shine btn-outline-danger border-0 btn-sm"
-                                                    type="submit" data-toggle="tooltip" title="Delete"
-                                                    data-placement="bottom"
-                                                    onclick="return confirm('Do you really want to delete this item?')">
-                                                            <span class="btn-icon-wrapper opacity-8">
-                                                                <i class="fa fa-trash fa-w-20"></i>
-                                                            </span>
-                                            </button>
-                                        </form>
                                     </td>
                                 </tr>
                             @endforeach
