e0615db8442333c76b65fa02a699126c85241833
Nguyễn Đình Hiếu <Hieu.iceTea@gmail.com>
2021-04-29 09:50:21 +0700

    UPDATE: Thêm hàm JS: addCart() dùng AJAX (vẫn load lại trang)

--- public/front/dist/js/custom.js
+++ public/front/dist/js/custom.js
@@ -1,8 +1,5 @@
 // Tùy biến JS
 
-
-
-
 $(document).ready(function () {
     $('.cart-summary').show();
     $('.notification').show();
@@ -11,3 +8,22 @@ $(document).ready(function () {
     $('#style-switcher-toggle').css('display', 'none');
     $('#style-switcher').css('display', 'none');
 });
+
+
+function addCart(productId) {
+    $.ajax({
+        type: "GET",
+        url: "../cart/add/" + productId,
+        data: {},
+        success: function (response) {
+            alert('Add successful!\nProduct: ')
+
+            location.reload(); //Tải lại trang
+            console.log(response);
+        },
+        error: function (response) {
+            alert('Add failed.');
+            console.log(response);
+        },
+    });
+}
