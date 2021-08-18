2f86e94c2ec9766e8fc64b53375728206eb00f34
Nguyễn Đình Hiếu <DinhHieu8896@gmail.com>
2021-08-18 12:05:07 +0700

    NEW: Facebook Messenger Plugin

--- resources/views/front/layout/master.blade.php
+++ resources/views/front/layout/master.blade.php
@@ -465,6 +465,37 @@
 <!-- JS Custom -->
 <script src="dist/js/custom.js"></script>
 
+
+
+<!-- Messenger Plugin chat Code -->
+<div id="fb-root"></div>
+
+<!-- Your Plugin chat code -->
+<div id="fb-customer-chat" class="fb-customerchat"
+     theme_color="#ddae71">
+</div>
+
+<script>
+    var chatbox = document.getElementById('fb-customer-chat');
+    chatbox.setAttribute("page_id", "107928264386009");
+    chatbox.setAttribute("attribution", "biz_inbox");
+
+    window.fbAsyncInit = function() {
+        FB.init({
+            xfbml            : true,
+            version          : 'v11.0'
+        });
+    };
+
+    (function(d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return;
+        js = d.createElement(s); js.id = id;
+        js.src = 'https://connect.facebook.net/vi_VN/sdk/xfbml.customerchat.js';
+        fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));
+</script>
+
 </body>
 
 
